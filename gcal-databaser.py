4cb3649ec954faa5ba88e83f91a605564bc3cb0c
claudiopacha <claudiomonteiro@protonmail.com>
2019-05-31 15:25:19 -0300

    t

--- gcal-databaser.py
+++ gcal-databaser.py
@@ -1,18 +1,17 @@
 '''
 TEXT MINING OF ICS FILE FROM GOOGLE CALENDAR
-'''
 
-
-'''
 Takes the ics file as input and 
 export a CSV dataset with the informations
 '''
-#def icsTableGenerator():
 
-
+# import modules
+import pandas as pd
 
 '''
-search for the keywords and append to var dictionary of list
+
+ search for the keywords and append to var dictionary of list
+
 '''
 
 file = 'claudiomonteirol.a@gmail.com.ics'
@@ -43,7 +42,7 @@ def listToData(file):
     }
 
     # loop for appending the infos to dict columns
-    for event in cal:
+    for event in cal[1:]: # this hide the first element wich is not a event, but hides the local timezone too
         for info in event:
             for key, value in data.items():
                 if key == 'SUMMARY':
@@ -53,17 +52,15 @@ def listToData(file):
                 else:
                     if key in info:
                         data[key].append(info)
+    # return data
     return data
 
 
-data = listToData(cal)
+# aply function to local file
+data = listToData('claudiomonteirol.a@gmail.com.ics')
 
-for key, value in data.items():
-    print(key, len(value))
+# transform dict in pd dataframe
+dataset = pd.DataFrame(data)
 
-count = 0
-for i in data['SUMMARY']:
-    if 'Alarm' not in i:
-        count += 1
-
-count
+# dataset head
+dataset.head()
